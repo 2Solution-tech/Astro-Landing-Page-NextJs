d2e587e9983cb4bc248fc3e19b930005df365501
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-10-21 23:57:07 +0530

    Fix some padding issues

--- src/component/FixedCall.jsx
+++ src/component/FixedCall.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 const FixedCallButton = () => {
     return (
-        <div className="fixed bottom-4 left-4 z-50 transition-transform transform-gpu hover:scale-110 animate-bounce w-[4rem] h-[4rem]">
+        <div className="fixed md:bottom-[2rem] bottom-[4rem] left-4 z-50 transition-transform transform-gpu hover:scale-110 animate-bounce w-[4rem] h-[4rem]">
           <Link href={`tel:${9499475794}`}>
           
             <button className="bg-blue-500 text-white  py-2 px-2 rounded-full cursor-pointer">
